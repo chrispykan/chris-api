0f55d2182c228d2d644dcfef1ccc0909452fa680
chrispykan <christineparisk@gmail.com>
2019-01-18 19:24:59 -0800

    connected to db

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
+
+
+
+require('dotenv').config();
+
+
+
+
 module.exports = app;
---
+++ db.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  'mongodb://localhost//personalapi'
+);
